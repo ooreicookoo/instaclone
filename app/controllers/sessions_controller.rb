8b79f2673dee43564679788bdf85e59b4fa17d22
Reiko Enomoto <ooreicookoo@gmail.com>
2020-10-08 22:14:03 +0900

    WIP:ログインに失敗したときはフラッシュメッセージ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user. authenticate(params[:session][:password])
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
