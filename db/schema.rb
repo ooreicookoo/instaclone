d6bcc6b73298940c3a2e66f0286ed9d44d3d96d8
Reiko Enomoto <ooreicookoo@gmail.com>
2020-10-12 17:28:06 +0900

    WIP:Feedにmigrationファイルにコンテンツを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_155633) do
+ActiveRecord::Schema.define(version: 2020_10_12_082215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "feeds", force: :cascade do |t|
     t.text "image"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
