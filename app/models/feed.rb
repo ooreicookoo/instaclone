d0dfefe361bc88b0f649b366b248884a8fc8dfc3
Reiko Enomoto <ooreicookoo@gmail.com>
2020-10-23 13:31:13 +0900

    WIP：画像サイズの設定

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,7 @@ class Feed < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
   validates :content, presence: true
 
   def favorited_by? user
