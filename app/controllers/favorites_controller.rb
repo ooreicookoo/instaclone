a39bdf4418072955a151cc5537fbc164752b8a5f
Reiko Enomoto <ooreicookoo@gmail.com>
2020-10-23 10:40:22 +0900

    WIPお気に入り一覧の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,17 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(feed_id: params[:feed_id])
+    favorite.save
     redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんの投稿に♡しました"
   end
 
   def destroy
     favorite = current_user.favorite.find_by(id: params[:id]).destroy
     redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんの投稿のいいね！を取り消しました"
+  end
+
+  def index
+    @user = current_user
+    @favorites = Favorite.where(user_id: @user.id).all
   end
 end
