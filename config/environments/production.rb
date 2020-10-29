c7913d73bee3b636d1c7608dc96b2a4fc72fa63a
Reiko Enomoto <ooreicookoo@gmail.com>
2020-10-29 15:23:07 +0900

    デプロイ前

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,20 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+
+  config.action_mailer.default_url_options = { host: 'shrouded-taiga-97335.herokuapp.com' }
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: "heroku.com",
+      address: "smtp.SendGrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+  end
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
@@ -92,3 +106,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+　　
